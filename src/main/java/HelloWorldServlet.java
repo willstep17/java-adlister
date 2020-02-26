15bdf0f618c06cdc182a30f9b2e38eb73ce6c0c5
William Stephens <william.a.stephens17@gmail.com>
2020-02-26 10:23:55 -0600

    Finished hello world set up.

--- src/main/java/HelloWorldServlet.java
+++ src/main/java/HelloWorldServlet.java
@@ -1,9 +1,25 @@
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
 
+@WebServlet(name = "HelloWorldServlet", urlPatterns = "/hello")
 public class HelloWorldServlet extends HttpServlet {
-    protected void doGet(HttpServletRequest req, HttpServletResponse res) {
-
+    private int counter = 0;
+    private String name = "Dylan";
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException, IOException {
+        response.setContentType("text/html");
+        PrintWriter out = response.getWriter();
+        if(name.equals(null) || name.equals("") || name.equals(" ")) {
+            out.println("<h1>Hello, World!</h1>");
+        }else{
+            out.println("<h1>Hello, "+ name + "!</h1>");
+        }
+        counter += 1;
+        out.println("<h1>The count is " + counter + ".</h1>");
     }
 }
