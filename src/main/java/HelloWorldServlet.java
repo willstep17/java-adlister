c801124b48048d3072812abb33f6b2f699b22804
William Stephens <william.a.stephens17@gmail.com>
2020-02-26 14:53:37 -0600

    Working on jsp exercises

--- src/main/java/HelloWorldServlet.java
+++ src/main/java/HelloWorldServlet.java
@@ -7,6 +7,13 @@ import java.io.IOException;
 @WebServlet(name = "HelloWorldServlet", urlPatterns = "/")
 public class HelloWorldServlet extends HttpServlet {
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
-        response.getWriter().println("<h1>Hello, World!</h1>");
+        String input = request.getParameter("input");
+        if(input.equals("first")) {
+            response.getWriter().println("<h1>First</h1>");
+        } else if(input.equals("second")) {
+            response.getWriter().println("<h1>Second</h1>");
+        } else {
+            response.getWriter().println("<h1>Hello, World!</h1>");
+        }
     }
 }
---
+++ src/main/webapp/login.jsp
@@ -0,0 +1,22 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: williamstephens
+  Date: 2/26/20
+  Time: 2:20 PM
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>Title</title>
+</head>
+<body>
+    <form method="POST" action="/login.jsp">
+        <label for="username">Username: </label>
+        <input id="username" name="username" type="text">
+        <label for="password">Password: </label>
+        <input id="password" name="password" type="password">
+        <input type="submit">
+    </form>
+</body>
+</html>
