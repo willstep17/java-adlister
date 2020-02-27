269c88ba247d2ee3bfb19c338fec67a0e213455d
William Stephens <william.a.stephens17@gmail.com>
2020-02-27 15:44:10 -0600

    establishing mvc-exercise

---
+++ src/main/java/LoginServlet.java
@@ -0,0 +1,18 @@
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet(name = "LoginServlet", urlPatterns = "/login.jsp")
+public class LoginServlet extends HttpServlet {
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
+        String username = request.getParameter("username");
+        String password = request.getParameter("password");
+        if ((request.getParameter("username") != null && request.getParameter("username").equals("admin")) && (request.getParameter("password").equals("password"))) {
+            response.sendRedirect("/profile.jsp");
+        }
+        request.getRequestDispatcher("/login.jsp").forward(request, response);
+    }
+}
