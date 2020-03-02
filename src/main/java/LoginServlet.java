28358627f3c2167213beeadd2c3c89291f1de41a
William Stephens <william.a.stephens17@gmail.com>
2020-03-02 09:37:41 -0600

    Redoing exercises

--- src/main/java/LoginServlet.java
+++ src/main/java/LoginServlet.java
@@ -5,7 +5,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
-@WebServlet(name = "LoginServlet", urlPatterns = "/login.jsp")
+@WebServlet(name = "LoginServlet", urlPatterns = "/login")
 public class LoginServlet extends HttpServlet {
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
         String username = request.getParameter("username");
